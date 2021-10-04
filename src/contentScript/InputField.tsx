9617001be611277abb8411e11589bb104d062b81
Erencan KARADAĞ <64350262+ekaradag14@users.noreply.github.com>
2021-10-04 18:20:41 +0300

    Simple Content Script Flow
    Simple content script flow established

---
+++ src/contentScript/InputField.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const InputField = `<div id="extra-chrome-gift">
+		<input type="text" placeholder="Ne gircen" style="display:absolute;"></input>
+		<button style="cursor:pointer;" type="button" id="anan-but-x" onClick="console.log(")">
+			X
+		</button>
+	</div>`;
+export default InputField;
--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,1 +1,29 @@
 // TODO: content script
+const changeableTags: string[] = ['a', 'p', 'em', 'b'];
+import InputField from './InputField';
+const tagTypeToBeChanged =
+	changeableTags[Math.floor(Math.random() * changeableTags.length)];
+const tags = document.getElementsByTagName(tagTypeToBeChanged);
+const getRandomArbitrary = (min: number, max: number) => {
+	return Math.random() * (max - min) + min;
+};
+
+const randomElementIndex: number = getRandomArbitrary(0, tags.length);
+const chosenItem = tags.item(randomElementIndex) as HTMLElement;
+
+chosenItem.style['background-color'] = 'yellow';
+// chosenItem.textContent = 'Selam Baba';
+chosenItem.innerHTML = `${InputField} ${chosenItem.innerHTML} `;
+
+//Create Clear Button Functionality
+let button = document.querySelector('#anan-but-x') as HTMLElement;
+button.addEventListener(
+	'click',
+	function () {
+		let div = document.querySelector('#extra-chrome-gift') as HTMLElement;
+		div.style['display'] = 'none';
+	},
+	false
+);
+
+console.log(tagTypeToBeChanged, chosenItem);
