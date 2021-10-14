e2ebd7b6f3fbbf570e21561c66ce6b3c27e68ba4
Erencan KARADAĞ <64350262+ekaradag14@users.noreply.github.com>
2021-10-14 17:58:59 +0300

    Port Connection Established

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,71 @@
-// TODO: background script
-chrome.runtime.onInstalled.addListener(() => {
-  // TODO: on installed function
-})
+const handleMessages = async (msg, sender, sendResponse) => {
+	if (msg.greeting === 'hello') {
+		console.log('hello', msg);
+		chrome.action.setBadgeText({ text: 'load' });
+		let reqData;
+		try {
+			reqData = await fetch(
+				`http://localhost:5001/shopgrid-0/us-central1/api/translate`,
+				{
+					method: 'POST',
+					body: JSON.stringify({ text: 'Selam' }),
+				}
+			);
+			reqData = (await reqData.json()).data;
+		} catch (error) {
+			console.error(error);
+			chrome.action.setBadgeText({ text: '' });
+			return;
+		}
+
+		console.log('reqData', reqData);
+		chrome.storage.local.get(['latestExecutions'], (res) => {
+			if (res.latestExecutions && reqData.data) {
+				chrome.storage.local.set({
+					latestExecutions: [reqData.data, ...res.latestExecutions.slice(0, 4)],
+				});
+			}
+		});
+
+		if (msg.greeting == 'hello') {
+			console.log('message sent');
+			sendResponse(reqData);
+		}
+		chrome.action.setBadgeText({ text: '' });
+	}
+	return true;
+};
+
+chrome.runtime.onMessage.addListener(handleMessages);
+
+// var port = chrome.tabs.connect({ name: 'knockknock' });
+// port.postMessage({ joke: 'Knock knock' });
+// port.onMessage.addListener(function (msg) {
+// 	if (msg.question === "Who's there?") port.postMessage({ answer: 'Madame' });
+// 	else if (msg.question === 'Madame who?')
+// 		port.postMessage({ answer: 'Madame... Bovary' });
+// });
+
+//Add a listener for any port messages
+chrome.runtime.onConnect.addListener(function (port) {
+	port.onMessage.addListener(async function (msg) {
+		let reqData;
+		try {
+			reqData = await fetch(
+				`http://localhost:5001/shopgrid-0/us-central1/api/translate`,
+				{
+					method: 'POST',
+					body: JSON.stringify({ text: 'Selam' }),
+				}
+			);
+			reqData = (await reqData.json()).data;
+		} catch (error) {
+			console.error(error);
+			chrome.action.setBadgeText({ text: '' });
+			return;
+		}
+		// if (msg.joke == 'Knock knock')
+		port.postMessage({ joke: reqData.translatedPrompts.en });
+		console.log('hey');
+	});
+});
--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -3,6 +3,18 @@ import './contentScript.css';
 const changeableTags: string[] = ['h1', 'h2', 'h3', 'h4', 'p', 'em', 'b'];
 import InputField from './InputField';
 
+type FirebaseResp = {
+	basePrompt: string;
+	id: string;
+	translatedPrompts: {
+		de: string;
+		en: string;
+		es: string;
+		fr: string;
+		tr: string;
+	};
+};
+
 const tagTypeToBeChanged =
 	changeableTags[Math.floor(Math.random() * changeableTags.length)];
 const tags = document.getElementsByTagName(tagTypeToBeChanged);
@@ -14,7 +26,7 @@ const randomElementIndex: number = getRandomArbitrary(0, tags.length);
 let chosenItem = tags.item(randomElementIndex) as HTMLElement;
 chosenItem = document.querySelector('#firstHeading');
 // chosenItem.style['background-color'] = 'yellow';
-let translateText = chosenItem.textContent;
+let translateText = chosenItem?.textContent;
 if (translateText) {
 	chosenItem.innerHTML = `${InputField} <em style="background-color: yellow">${chosenItem.innerHTML}</em> `;
 }
@@ -38,22 +50,45 @@ let translateButton = document.querySelector(
 ) as HTMLElement;
 translateButton.addEventListener(
 	'click',
-	function (e) {
+	async function (e) {
 		e.preventDefault();
 		let elem = document.querySelector('.ern-ext-input') as HTMLInputElement;
 		let value = elem.value;
 		// document.querySelector('.extra-chrome-gift').remove();
 		document.querySelector('.ern-ext-translate-button').className += ' clicked';
 
-		setTimeout(() => {
-			document
-				.querySelector('.ern-ext-translate-button')
-				.classList.remove('clicked');
-			setTimeout(() => {
-				chosenItem.innerHTML =
-					'<em style="background-color: yellow">' + value + '</em>';
-			}, 500);
-		}, 1000);
+		//create a port to send messages throughout the application
+		var port = chrome.runtime.connect({
+			name: 'knockknock',
+		});
+		//Send a message from the port
+		port.postMessage({ joke: 'Knock knock' });
+		//Handle any response that might come on this port
+		port.onMessage.addListener(function (msg) {
+			console.log('helu');
+
+			changeContent(msg.joke);
+		});
 	},
 	false
 );
+
+const changeContent = (value) => {
+	document
+		.querySelector('.ern-ext-translate-button')
+		.classList.remove('clicked');
+	setTimeout(() => {
+		chosenItem.innerHTML =
+			'<em style="background-color: yellow">' + value + '</em>';
+	}, 500);
+};
+
+// Add a listener for any port that can be opened in the application
+chrome.runtime.onConnect.addListener(function (port) {
+	console.assert(port.name === 'knockknock');
+	//Handle any message that can come
+	port.onMessage.addListener(function (msg) {
+		if (msg.joke === 'Knock knock')
+			port.postMessage({ question: "Who's there?" });
+	});
+});
